bece494704a9d935a01e0dbc794528e7f7d994b1
jssujjaval <ujjaval.shelat@gmail.com>
2015-03-30 18:16:42 +0530

    Create Graph.java

---
+++ com/jss/baggage/model/Graph.java
@@ -0,0 +1,22 @@
+package com.jss.baggage.model;
+
+import java.util.List;
+
+public class Graph {
+	private final List<Node> vertexes;
+	private final List<Edge> edges;
+
+	public Graph(List<Node> vertexes, List<Edge> edges) {
+		this.vertexes = vertexes;
+		this.edges = edges;
+	}
+
+	public List<Node> getVertexes() {
+		return vertexes;
+	}
+
+	public List<Edge> getEdges() {
+		return edges;
+	}
+
+}
